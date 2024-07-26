45a8c51eb3f9f17d66d54398c885a089ac3a286f
VALENTINA AVILA RAMIREZ <VALENTINA.AVILA@helisa.local>
2024-07-26 17:12:01 -0500

    Date

--- Date/Funciones_fechas.php
+++ Date/Funciones_fechas.php
@@ -1,17 +1,15 @@
 <?php
 
 //Establecer zona horaria
-
 date_default_timezone_set("America/Bogota");
-
+ 
 
 //Obtener fecha actual
-
 $now = date("Y-m-d H:i:s");     //String
 $now = date_create();           //Objeto
 
-//Strtotime combierte string en formato Unix
 
+//Strtotime combierte string en formato Unix
 echo strtotime($now);
 echo strtotime("17 April 2022");
 echo strtotime("next Monday");
@@ -19,8 +17,26 @@ echo strtotime("next Monday");
 $unix_time = strtotime("20 April 2022");
 echo date("Y-m-d H:i:s", $unix_time);
 
-//Fechas inmutables
 
+//Fechas inmutables
 $date_imnmutables = new DateTimeImmutable();
+
+
+//Diferencia de tiempo
+$today = new DateTime();
+$birth = new DateTime("2006-03-20");
+$interval = $birth->diff($today);
+echo $interval ->format("%y años con %d dias");
+
+
+//Crear desde un formato
+$date = DateTime::createFromFormat("l j F Y", "Sunday 17 April 2023");
+echo $date->format("Y-m-d H:i:s");
+
+
+//Modificar una fecha
+$date = new DateTime();
+$date ->modify("+1 day +2 months");
+echo $date->format("Y-m-d");
 
 ?>
