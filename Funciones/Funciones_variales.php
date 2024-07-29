163f5d3a5bb2bf29b04dc3412f305858360ccb34
VALENTINA AVILA RAMIREZ <VALENTINA.AVILA@helisa.local>
2024-07-29 12:04:00 -0500

    Funciones Variables

---
+++ Funciones/Funciones_variales.php
@@ -0,0 +1,19 @@
+<?php
+function saludar_ana() {
+    echo "Hola Ana";
+}
+
+function saludar_juan() {
+    echo "Hola Juan";
+}
+
+function saludar_jose() {
+    echo "Hola Jose";
+}
+
+$funtion =readline("Escriba la funcion:");
+
+$function();
+
+
+?>
