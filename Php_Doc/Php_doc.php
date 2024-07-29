1c2cb3d2ba016131e4d9d4232b2361a2b62c26ba
VALENTINA AVILA RAMIREZ <VALENTINA.AVILA@helisa.local>
2024-07-29 09:08:39 -0500

    Php Doc

---
+++ Php_Doc/Php_doc.php
@@ -0,0 +1,16 @@
+<?
+
+/**
+ * Función de suma
+ * @param int $n1
+ * @param int $n2
+ * @return int
+ */
+
+function sum ($n1 , $n2){
+    return $n1 +$n2;
+}
+
+$resultado = sum($n1, $n2);
+echo $resultado;
+
